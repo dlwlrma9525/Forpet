eaf9caee326bc6ac2874ed038b98a90c95f79465
DESKTOP-J2D8T70\iudev <iudeveloment@gmail.com>
2019-09-10 16:12:41 +0900

    add popup view option images..

--- app/src/main/java/kr/forpet/view/main/adapter/PopupViewPagerAdapter.java
+++ app/src/main/java/kr/forpet/view/main/adapter/PopupViewPagerAdapter.java
@@ -1,12 +1,15 @@
 package kr.forpet.view.main.adapter;
 
 import android.content.Context;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.viewpager.widget.PagerAdapter;
@@ -32,26 +35,45 @@ public class PopupViewPagerAdapter extends PagerAdapter {
         // super.instantiateItem(container, position);
         LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View itemView = inflater.inflate(R.layout.layout_popup, container, false);
-
-        TextView textDistance = itemView.findViewById(R.id.text_popup_distance);
-        TextView textPlaceName = itemView.findViewById(R.id.text_popup_place);
-        TextView textAddressName = itemView.findViewById(R.id.text_popup_address);
-        Button buttonNavigation = itemView.findViewById(R.id.button_popup_navigation);
-        Button buttonCall = itemView.findViewById(R.id.button_popup_call);
+        itemView.setOnClickListener((v) -> onItemListener.onItemClick(mList.get(position)));
 
         Shop shop = mList.get(position);
 
+        TextView textDistance = itemView.findViewById(R.id.text_popup_distance);
         textDistance.setText(String.format("%.2f km", shop.getDistance() / 1000));
+
+        TextView textPlaceName = itemView.findViewById(R.id.text_popup_place);
         textPlaceName.setText(shop.getPlaceName());
+
+        TextView textAddressName = itemView.findViewById(R.id.text_popup_address);
         textAddressName.setText(shop.getRoadAddressName());
 
-        itemView.setOnClickListener((v) -> {
-            onItemListener.onItemClick(mList.get(position));
-        });
+        LinearLayout linearLayout = itemView.findViewById(R.id.linear_popup_option);
 
+        if (shop.getOptParking() != null)
+            linearLayout.addView(createImageView(R.drawable.enable_parking));
+        if (shop.getOptReservation() != null)
+            linearLayout.addView(createImageView(R.drawable.enable_reservation));
+        if (shop.getOptWifi() != null)
+            linearLayout.addView(createImageView(R.drawable.enable_wifi));
+        if (shop.getOpt365().equals("Y"))
+            linearLayout.addView(createImageView(R.drawable.enable_365));
+        if (shop.getOptNight().equals("Y"))
+            linearLayout.addView(createImageView(R.drawable.enable_night));
+        if (shop.getOptShop().equals("Y"))
+            linearLayout.addView(createImageView(R.drawable.enable_shop));
+        if (shop.getOptBeauty().equals("Y"))
+            linearLayout.addView(createImageView(R.drawable.enable_beauty));
+        if (shop.getOptBigdog().equals("Y"))
+            linearLayout.addView(createImageView(R.drawable.enable_bigdog));
+        if (shop.getOptHotel().equals("Y"))
+            linearLayout.addView(createImageView(R.drawable.enable_hotel));
+
+        Button buttonNavigation = itemView.findViewById(R.id.button_popup_navigation);
         buttonNavigation.setOnClickListener((v) -> {
         });
 
+        Button buttonCall = itemView.findViewById(R.id.button_popup_call);
         buttonCall.setOnClickListener((v) -> {
         });
 
@@ -72,6 +94,22 @@ public class PopupViewPagerAdapter extends PagerAdapter {
     @Override
     public int getCount() {
         return mList.size();
+    }
+
+    private ImageView createImageView(int resourceId) {
+        FrameLayout.LayoutParams params
+                = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        DisplayMetrics metrics = mContext.getResources().getDisplayMetrics();
+        params.height = Math.round(21 * metrics.density);
+        params.width = Math.round(21 * metrics.density);
+        params.setMarginEnd(Math.round(5 * metrics.density));
+
+        ImageView image = new ImageView(mContext);
+        image.setImageResource(resourceId);
+        image.setLayoutParams(params);
+
+        return image;
     }
 
     private OnItemListener onItemListener;
